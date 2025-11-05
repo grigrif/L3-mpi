d7a92212a146f95d45c13c8d690ef8914a0da063
grigrif <grigri598@gmail.com>
2025-11-05 10:52:07 +0100

    a

--- main.c
+++ main.c
@@ -1,12 +1,8 @@
+#include "add.h"
 #include <stdio.h>
-
 int main(void) {
-  printf("Hello world !");
-  // Super COmmentaire 
-  int d;
-  scanf("%d", &d);
-
-  printf("Vous avez entré l'entier %d!", d);
-
+  int x = 17;
+  int y = 25;
+  printf("%d + %d = %d\n", x, y, add(x, y));
   return 0;
 }
